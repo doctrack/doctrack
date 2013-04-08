02cc32855a088074fbbc0b724eab75157e9967d3
suras <msurendar@outlook.com>
2013-04-08 18:48:22 +0530

    Initial App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctrack::Application.config.secret_token = '8e93bf3679aa67f0073cab7b0bb68324c76f08db398752ba552d8d1ec0d63d7e9c645c328d9fa27ab2c64a5ecee4d56b7e037a5a0772c843abb30996d3a883ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Doctrack::Application.config.session_store :cookie_store, key: '_doctrack_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Doctrack::Application.config.session_store :active_record_store
