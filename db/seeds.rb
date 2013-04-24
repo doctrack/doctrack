4e231f1d15bdc4f38bc685339f04caa0dfc9d5d0
suras <msurendar@outlook.com>
2013-04-24 21:01:19 +0530

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+puts 'username: ' << user.name
